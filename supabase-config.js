b3ee0ad9ec4de068a8b4d2ddc67812d73b0ad1aa
RideChalo <support.ridechalo@gmail.com>
2026-09-21 21:30:29 +0530

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -13,7 +13,7 @@ const SUPABASE_URL =
   "https://xocfwbrqxllqyufjddcv.supabase.co";
 
 const SUPABASE_ANON_KEY =
-  "sb_publishable_p45SbnHnuq8ed7SSZzBLOQ_ezEvaBHA";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhvY2Z3YnJxeGxscXl1ZmpkZGN2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk5Njk2OTQsImV4cCI6MjEwNTU0NTY5NH0.QcNN01TbDLW6m9xAs4Wl1I7OYpb6ZjQE3zlTwKszN_A";
 
 let supabaseClient = null;
 
