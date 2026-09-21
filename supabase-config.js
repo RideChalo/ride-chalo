aa94819d113cd413e79ddf17a357a8659c401f49
RideChalo <support.ridechalo@gmail.com>
2026-09-21 20:42:21 +0530

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,22 +1,19 @@
 /*
   RIDE CHALO - SUPABASE CONFIG
 
-  This file connects the frontend to Supabase.
-
-  Authentication can support:
+  Authentication:
   1. Mobile / Phone OTP
   2. Email verification
 
   IMPORTANT:
-  Use only the Supabase Publishable/Anon key here.
-  NEVER put the Supabase service_role/secret key in this file.
+  Use only Supabase Publishable/Anon key here.
 */
 
 const SUPABASE_URL =
   "https://xocfwbrqxllqyufjddcv.supabase.co";
 
 const SUPABASE_ANON_KEY =
-  "sb_publishable_p45SbnHnuq8ed7SSZzBLOQ_ezEvaBHA";
+  "sb_publishable_p45SbnHnuq8ed7SSZzBLOQ_ezEvaBHA0NTY5NH0.QcNN01TbDLW6m9xAs4Wl1I7OYpb6ZjQE3zlTwKszN_A";
 
 let supabaseClient = null;
 
@@ -24,7 +21,7 @@ try {
   if (
     window.supabase &&
     SUPABASE_URL.startsWith("https://") &&
-    SUPABASE_ANON_KEY!== "sb_publishable_p45SbnHnuq8ed7SSZzBLOQ_ezEvaBHA"
+    SUPABASE_ANON_KEY
   ) {
     supabaseClient = window.supabase.createClient(
       SUPABASE_URL,
